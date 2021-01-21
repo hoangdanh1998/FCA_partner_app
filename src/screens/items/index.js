99d693e3d05a1c224d5c083064caa9cdf8495283
pkimanh03 <pkimanh03@gmail.com>
2021-01-21 22:54:15 +0700

    upcoming order card component

--- src/screens/items/index.js
+++ src/screens/items/index.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Text, ScrollView } from 'react-native';
-import SearchBar from '../../components/atoms/SearchBar';
-import useResults from '../../hooks/useResults';
-import ResultsList from '../../components/molecules/ResultsList';
+import React, { useState } from "react";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
+import SearchBar from "../../components/atoms/SearchBar";
+import useResults from "../../hooks/useResults";
+import ResultsList from "../../components/molecules/ResultsList";
+import OrderCardUpComing from "../../components/molecules/OrderCardUpComing/OrderCardUpComing";
 
 const SearchScreen = () => {
+  const [errorMessage, results, searchApi] = useResults();
+  const [term, setTerm] = useState("");
 
-    const [errorMessage, results, searchApi] = useResults();
-    const [term, setTerm] = useState('');
+  const filterResultsByPrice = (price) => {
+    //price === '$' or === '$$'' or === '$$$'
+    return results.filter((results) => {
+      return results.price === price;
+    });
+  };
 
-    const filterResultsByPrice = price => {
-        //price === '$' or === '$$'' or === '$$$'
-        return results.filter(results => {
-            return results.price === price;
-        });
-    };
-
-    return (
-        <View style = {{flex: 1}}>
-            <SearchBar
+  return (
+    <View style={{ flex: 1 }}>
+      {/* <SearchBar
                 term={term}
                 onTermChange={newTerm => setTerm(newTerm)}
                 onTermSubmit={() => searchApi(term)}
@@ -33,20 +33,10 @@ const SearchScreen = () => {
                 />
                 <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricer" />
                 <ResultsList results={filterResultsByPrice('$')} title="Big Spender" />
-            </ScrollView>
-
-
-        </View>
-    );
-
+            </ScrollView> */}
+      <OrderCardUpComing />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
-
-});
-
-
 export default SearchScreen;
-
-
-
