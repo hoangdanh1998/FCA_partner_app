745102511ad2c8cf78a0f7a4c89c9e74e54ff44b
Hoang Danh <danh02071998@gmail.com>
2021-03-02 23:12:33 +0700

    Listen order

--- src/firebase/realtime-database/listener.js
+++ src/firebase/realtime-database/listener.js
@@ -1,6 +1,6 @@
 import * as firebase from 'firebase'
 export const listenOrder = async (orderId, returnValue) => {
-    firebase.database().ref(orderId).on('value', (snapshot) => {
+    firebase.database().ref('order').child(orderId).on('value', (snapshot) => {
         returnValue(snapshot.val().timeRemain)
     });
 }
