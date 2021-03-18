a19a3693c22e150889f1ab606c0d2ab6c6c4f5bc
Van Khanh Bui <khanhbvse@gmail.com>
2021-03-18 23:00:03 +0700

    merge

--- src/components/atoms/cart-statistics/index.js
+++ src/components/atoms/cart-statistics/index.js
@@ -1,31 +1,45 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { ButtonColor, StatisticColor } from '../../../constance/constance';
 import { styles } from './style';
 
 export default function CartItemStatistic(props) {
     const { item } = props;
+    const setColorForText = (title) => {
+        switch (title) {
+            case "Hoàn tất":
+                return ButtonColor.ACCESSS;
+            case "Từ chối":
+                return ButtonColor.REJECTION;
+            case "Huỷ":
+                return StatisticColor.CANCELLATION;
+        }
+    }
+
     return (
-        <View style={[styles.container, styles.rowContainer]}>
-            <View style={styles.rowContainer}>
+        <View style={[styles.container]}>
+            <View style={[styles.rowContainer]}>
                 <Text
-                    style={styles.titleText}
+                    style={[styles.titleText, { color: setColorForText(item.title) }]}
                 >
                     {item.title}
                 </Text>
             </View>
             <View style={styles.rowContainer}>
                 <Text
-                    style={styles.titleText}
+                    style={[styles.titleText, { color: setColorForText(item.title) }]}
                 >
                     {item.number}
+                    {/* 100000000000000 */}
                 </Text>
-                <Text>đơn</Text>
+                <Text style = {{color: setColorForText(item.title)}}>đơn</Text>
             </View>
             <View style={styles.rowContainer}>
                 <Text
-                    style={styles.titleText}
+                    style={[styles.titleText, { color: setColorForText(item.title), textAlign:  }]}
                 >
                     {item.money}
+                    {/* 1000000000000 */}
                 </Text>
                 <Text>VNĐ</Text>
             </View>
